53cd52f57d576514096d090aa39235ced295518f
yuleivsc@gmail.com <yuleivsc@gmail.com@687fbbf2-21f2-c8b0-c2d8-3de74bb740c1>
2011-03-23 22:03:45 +0000

    V0.4.9

--- trunk/mk.sh
+++ trunk/mk.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd joomla_weibo
-zip -r com_weibo.zip com_weibo
+rm com_weibo.zip
+rm plugin_weibo.zip
+zip -r com_weibo.zip com_weibo -i \*.php \*.xml \*.ini
+zip -r plugin_weibo.zip plugin_weibo -i \*.php \*.xml \*.ini
